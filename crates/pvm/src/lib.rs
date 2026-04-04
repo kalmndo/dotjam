e1e1317ea32decb42222f1bf4c009203981b56a1
Adam Kalamondo <kalmndo@gmail.com>
2026-04-04 21:53:36 +0700

    opcode 51 register

--- crates/pvm/src/lib.rs
+++ crates/pvm/src/lib.rs
@@ -4,7 +4,7 @@ pub enum ExitReason {
     OutOfGas,
     Halt(u32),
     HostCall,
-    Fault(u32)
+    Fault(u32),
 }
 
 pub struct Machine {
@@ -15,7 +15,7 @@ pub struct Machine {
     exit: Option<ExitReason>,
     c: Vec<u8>,
     k: Vec<u8>,
-    j: Vec<u32>
+    j: Vec<u32>,
 }
 
 impl Machine {
@@ -28,7 +28,7 @@ impl Machine {
             exit: None,
             c: Vec::new(),
             k: Vec::new(),
-            j: Vec::new()
+            j: Vec::new(),
         }
     }
 
@@ -36,6 +36,10 @@ impl Machine {
         self.c = c;
         self.k = k;
         self.j = j;
+    }
+
+    pub fn register(&self, index: usize) -> u64 {
+        self.registers[index]
     }
 
     pub fn step(&mut self) -> Option<ExitReason> {
@@ -47,11 +51,39 @@ impl Machine {
         // gate 3: execute
         let opcode = self.c[self.pc as usize];
 
+        let skip = self.skip(self.pc);
+        let next_pc = self.pc + 1 + skip;
+
         match opcode {
             0 => return Some(ExitReason::Panic),
-            1 => { self.pc += 1; return None },
-            _ => return Some(ExitReason::Panic)
+            1 => {
+                self.pc = next_pc;
+                return None;
+            }
+            51 => {
+                let r_a = self.c[(self.pc + 1) as usize] % 16; // lower nibble = register index
+                let num_bytes = skip - 1;
+                let mut value = 0;
+
+                for i in 0..num_bytes {
+                    value |= (self.c[(self.pc + 2 + i) as usize] as u64) << (8 * i)
+                }
+
+                self.registers[r_a as usize] = value;
+                self.pc = next_pc;
+                return None;
+            }
+            _ => return Some(ExitReason::Panic),
         }
+    }
+
+    fn skip(&self, pc: u32) -> u32 {
+        let start = pc as usize + 1;
+        let mut i = start;
+        while i < self.k.len() && self.k[i] == 0 {
+            i += 1
+        }
+        (i - start) as u32
     }
 }
 
@@ -65,5 +97,13 @@ mod test {
         m.load_program(vec![0], vec![1], vec![]);
         let result = m.step();
         assert_eq!(result, Some(ExitReason::Panic));
+    }
+
+    #[test]
+    fn load_imm_loads_value() {
+        let mut m = Machine::new(1000);
+        m.load_program(vec![51, 0x00, 42], vec![1, 0, 0], vec![]);
+        m.step();
+        assert_eq!(m.register(0), 42)
     }
 }
